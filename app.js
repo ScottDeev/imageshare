2073f4e73b16a981d5ceccd789a0e9238b14f284
ScottDeev <99127718+ScottDeev@users.noreply.github.com>
2022-07-25 15:36:30 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,23 +1,34 @@
 
-document.getElementById('share').addEventListener('click', async () => {
-    const files = document.querySelector('img')
-    console.log(files);
-    if (!navigator.canShare) {
-        console.log('cant share');
-        return
-    }
+const input = document.getElementById('files')
+const output = document.getElementById('output')
 
-    if (navigator.canShare( {files} )) {
-        try {
-        await navigator.share({
-            files,
-            title: 'Images',
-            text: 'Beautiful images'
-        })
-        } catch (error) {
-            console.log(error);
-        }
-    } else {
-        console.log("cannot work");
+document.getElementById('share').addEventListener('click', async () => {
+  const files = input.files
+
+  if (files.length === 0) {
+    output.textContent = 'No files selected.'
+    return
+  }
+
+  // feature detecting navigator.canShare() also implies
+  // the same for the navigator.share()
+  if (!navigator.canShare) {
+    output.textContent = `Your browser doesn't support the Web Share API.`
+    return
+  }
+
+  if (navigator.canShare({ files })) {
+    try {
+      await navigator.share({
+        files,
+        title: 'Images',
+        text: 'Beautiful images'
+      })
+      output.textContent = 'Shared!'
+    } catch (error) {
+      output.textContent = `Error: ${error.message}`
     }
+  } else {
+    output.textContent = `Your system doesn't support sharing these files.`
+  }
 })
